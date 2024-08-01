b2791b8e0f3aebc755ccb01cfa66d81e009497cb
mosescode1 <emosescode@gmail.com>
2024-08-01 15:04:42 +0100

    mixed annotation using typing

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Mixed comples types Annotations"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """ype-annotated function sum_mixed_list which takes a
+    list mxd_lst of integers and floats and returns
+    their sum as a float."""
+    return float(sum(mxd_lst))
