cbf7ab5e0f479ce308d98edf6e9e210c3ff261ef
mosescode1 <emosescode@gmail.com>
2024-08-01 17:44:19 +0100

    done

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
 from typing import Any, Union, Sequence, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Union[Sequence[Any]], None]:
+  """"sumary_line"""
+
     if lst:
         return lst[0]
     else:
