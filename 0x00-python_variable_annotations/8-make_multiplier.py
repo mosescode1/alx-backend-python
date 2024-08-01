71fd8e97b37d90020c15638c4bbdc82a5298627e
mosescode1 <emosescode@gmail.com>
2024-08-01 15:44:36 +0100

    return

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function for a mul"""
+    def func(mul: float) -> float:
+        return mul * multiplier
+    return func
