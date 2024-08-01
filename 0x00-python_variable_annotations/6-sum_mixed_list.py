9d45c20622692318ffa50cb0838b73f72d509ba3
mosescode1 <emosescode@gmail.com>
2024-08-01 15:08:28 +0100

    Union type annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Mixed comples types Annotations"""
-from typing import List
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ype-annotated function sum_mixed_list which takes a
     list mxd_lst of integers and floats and returns
     their sum as a float."""
