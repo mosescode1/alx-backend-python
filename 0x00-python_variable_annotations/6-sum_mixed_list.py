dbfe45d61cf329dc2a4ef234d9f75f4c52141370
mosescode1 <emosescode@gmail.com>
2024-08-01 15:24:51 +0100

    complex mixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@ from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ype-annotated function sum_mixed_list which takes a
+    """type-annotated function sum_mixed_list which takes a
     list mxd_lst of integers and floats and returns
     their sum as a float."""
     return float(sum(mxd_lst))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Mixed comples types Annotations"""
+
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ype-annotated function sum_mixed_list which takes a
+    list mxd_lst of integers and floats and returns
+    their sum as a float."""
     square: float = v * v
     return tuple([k, square])
