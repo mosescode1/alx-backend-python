816c4f3e4b3640f2b5f252fda6981f3aee6cb25d
mosescode1 <emosescode@gmail.com>
2024-08-01 14:30:50 +0100

    Basic Annotation concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatination of two strings
+
+    Keyword arguments:
+    argument -- str1:str , str2:str
+    Return: str1 + str2
+    """
+    return str1 + str2
