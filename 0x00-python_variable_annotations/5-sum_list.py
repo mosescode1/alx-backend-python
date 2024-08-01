e5992d4ba37d990e08dc0bc8e220fafb51d836b8
mosescode1 <emosescode@gmail.com>
2024-08-01 14:58:52 +0100

    Using Typing for annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List) -> float:
     """Takes in a list and sum them to float"""
     return float(sum(input_list))
