437e023fb3d4a8d33deea2842e2b865c521dbbe0
mosescode1 <emosescode@gmail.com>
2024-08-05 16:23:56 +0100

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """Let's execute multiple coroutines at the same time with async"""
 from typing import List
+import asyncio
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Let's execute multiple coroutines at the same time with async"""
-    arr = []
-    for _ in range(n):
-        data = await task_wait_random(max_delay)
-
-        arr.append(data)
-    return arr
+    runs = [task_wait_random(max_delay) for _ in range(n)]
+    return [await con for con in asyncio.as_completed(runs)]
