bb11a8138dafda0ccc52c5feb1b61e7a697813ce
mosescode1 <emosescode@gmail.com>
2024-08-05 15:55:56 +0100

    done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Let's execute multiple coroutines at the same time with async"""
-from typing import Any, Dict, Optional, Union
 import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Let's execute multiple coroutines at the same time with async"""
     return asyncio.Task(wait_random(max_delay))
