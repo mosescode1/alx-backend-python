bdb7ec894b27a408069675ed4997494a628e5dcd
mosescode1 <emosescode@gmail.com>
2024-08-05 14:35:03 +0100

    done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,9 @@
 import asyncio
 from random import uniform
 
-if __name__ == "__main__":
 
-    async def wait_random(max_delay=10) -> float:
-        """A couratine for async function"""
-        random = uniform(0, max_delay)
-        await asyncio.sleep(random)
-        return random
+async def wait_random(max_delay=10) -> float:
+    """A couratine for async function"""
+    random = uniform(0, max_delay)
+    await asyncio.sleep(random)
+    return random
