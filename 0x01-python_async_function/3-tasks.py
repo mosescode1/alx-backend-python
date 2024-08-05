f3b665324c75fe328136632cc5ca05aaa8e7d7c1
mosescode1 <emosescode@gmail.com>
2024-08-05 15:54:51 +0100

    done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Let's execute multiple coroutines at the same time with async"""
+from typing import Any, Dict, Optional, Union
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> Any:
+    """Let's execute multiple coroutines at the same time with async"""
+    return asyncio.Task(wait_random(max_delay))
--- 0x01-python_async_function/test.py
+++ 0x01-python_async_function/test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-'''
-Test file for printing the correct output of the wait_n coroutine
-'''
 import asyncio
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
+
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
