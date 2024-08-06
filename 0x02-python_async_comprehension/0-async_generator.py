c8b3b0df4cd6572d04d7468cd1008cb61c734cbe
mosescode1 <emosescode@gmail.com>
2024-08-06 12:05:07 +0100

    done

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Async Generator"""
 import asyncio
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator"""
     for _ in range(1, 10):
         yield random.uniform(0, 10)
