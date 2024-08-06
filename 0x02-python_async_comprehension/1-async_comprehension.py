84ad7c3ddbfeacdd288419af3c9e5ef91d09ebac
mosescode1 <emosescode@gmail.com>
2024-08-06 12:01:02 +0100

    done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Aysnc Comprehension"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Aysnc Comprehension"""
+    return [x async for x in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""""""
+from time import perf_counter
+import asyncio
+async_comprehension = __import__(
+    '1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start = perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    time = perf_counter() - start
+    return time
