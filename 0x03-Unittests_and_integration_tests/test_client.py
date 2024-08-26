852266fee73ecadfd1924e70b27c93d57084b0f5
mosescode1 <emosescode@gmail.com>
2024-08-26 16:30:10 +0100

    set up pycodestyle

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,10 +9,13 @@ from utils import get_json
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for github organization client"""
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
+
+    @parameterized.expand(
+        [
+            ("google",),
+            ("abc",),
+        ]
+    )
     @patch("client.get_json")
     def test_org(self, org_name, mock_org):
         """Method ot test"""
@@ -22,32 +25,43 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_org.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
 
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
+    @parameterized.expand(
+        [
+            ("google",),
+            ("abc",),
+        ]
+    )
     def test_public_repos_url(self, org_name):
         """Mocking a property"""
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mocked_property:
+        with patch.object(
+            GithubOrgClient, "_public_repos_url", new_callable=PropertyMock
+        ) as mocked_property:
             obj = GithubOrgClient(org_name)
             mocked_property.return_value = "this is a value"
             self.assertEqual(obj._public_repos_url,
                              mocked_property.return_value)
             mocked_property.assert_called_once()
 
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
+    @parameterized.expand(
+        [
+            ("google",),
+            ("abc",),
+        ]
+    )
     @patch("client.get_json")
     def test_public_repos(self, org_name, mock_get_json):
         """Testing public repos"""
         mock_get_json.return_value = [
-            {"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
 
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mocked_property:
+        with patch.object(
+            GithubOrgClient, "_public_repos_url", new_callable=PropertyMock
+        ) as mocked_property:
             obj = GithubOrgClient(org_name)
             return_value = ["repo1", "repo2", "repo3"]
             self.assertEqual(obj.public_repos(), return_value)
             mock_get_json.assert_called_once()
-            mocked_property.assert_called_once()
+            mocked_property.assert_called_once
