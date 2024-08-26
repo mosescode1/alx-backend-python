a686e75fe1fffd0d71804a9251cc1d4dee610021
mosescode1 <emosescode@gmail.com>
2024-08-26 14:57:31 +0100

    changed naming convention

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -74,9 +74,9 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(TestClass, "a_method") as mock_method:
-            mock = TestClass()
-            mock.a_property()
-            mock.a_property()
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
             mock_method.assert_called_once()
 
 
