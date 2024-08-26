8aee63235427836a3381d72ca1a14e272d1a4061
mosescode1 <emosescode@gmail.com>
2024-08-26 15:01:26 +0100

    revert back to check if it works

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,25 +59,33 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """Testing for memorization"""
+    class TestClass:
+        """Test case for"""
 
-    def test_memonize(self):
-        """Testing  for memonize"""
+        def a_method(self):
+            return 42
 
-        class TestClass:
-            """Test case for memonize"""
+        @memoize
+        def a_property(self):
+            return self.a_method()
 
-            def a_method(self):
-                return 42
+    @patch.object(TestClass, "a_method")
+    def test_memoize(self, mock_methd):
 
-            @memoize
-            def a_property(self):
-                return self.a_method()
+        mock = self.TestClass()
+        # mock_methd.return_value = 42
 
-        with patch.object(TestClass, "a_method") as mock_method:
-            test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            mock_method.assert_called_once()
+        # First call to a_property should call a_method
+        result1 = mock.a_property
+        mock_methd.assert_called_once()
+
+        # Second call to a_property should not call a_method again
+        result2 = mock.a_property
+        mock_methd.assert_called_once()  # Should still be called only once
+
+        # # Ensure the property returns the correct value
+        # self.assertEqual(result1, 42)
+        # self.assertEqual(result2, 42)
 
 
 if __name__ == "__main__":
