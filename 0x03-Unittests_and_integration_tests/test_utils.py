3cee8cc31492a7105dc726987c86b32130d476c2
mosescode1 <emosescode@gmail.com>
2024-08-26 14:15:54 +0100

    Testcase for memonization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Moduule Test Case"""
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
@@ -55,6 +55,38 @@ class TestGetJson(unittest.TestCase):
         user_data = get_json(url)
 
         self.assertEqual(user_data, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Testing for memorization"""
+    class TestClass:
+        """Test case for memonize"""
+
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    @patch.object(TestClass, "a_method")
+    def test_memonize(self, mock_methd):
+        """Testing  for memonize"""
+
+        mock = self.TestClass()
+        mock_methd.return_value = 42
+
+        # First call to a_property should call a_method
+        result1 = mock.a_property
+        mock_methd.assert_called_once()
+
+        # Second call to a_property should not call a_method again
+        result2 = mock.a_property
+        mock_methd.assert_called_once()  # Should still be called only once
+
+        # Ensure the property returns the correct value
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
 
 
 if __name__ == "__main__":
