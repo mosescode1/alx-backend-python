b9000203d1ebe4149d6e79e7c7c10800972d2e35
mosescode1 <emosescode@gmail.com>
2024-08-26 13:04:32 +0100

    Add a testcase checking if error is being raised when a wrong key is being passed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,17 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(
             nested_map=nested_map, path=path), expected)
 
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({"a": 1}, ('a', 'b'))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test for Raised error"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
 
 if __name__ == "__main__":
     unittest.main()
